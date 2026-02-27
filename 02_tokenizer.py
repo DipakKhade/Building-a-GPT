3249b57b319f6e4438738651c4937ee378ea3615
Dipak Khade <dipakhade214@gmail.com>
2026-02-27 19:11:20 +0530

    made all methods to be static

--- 02_tokenizer.py
+++ 02_tokenizer.py
@@ -1,23 +1,29 @@
-
+from typing import List
 # custom simple tokenzier  #dipak
 class CustomTokenzier:
+    trainig_data = ''
+    unique_chars = ''
     def __init__(self, trainig_data_file_path):
         with open(trainig_data_file_path, 'r+') as f:
             content = f.read()
-            self.trainig_data = content
+            CustomTokenzier.trainig_data = content
+            CustomTokenzier.unique_chars = CustomTokenzier.get_all_unique_chars_from_traning_txtdata(CustomTokenzier.trainig_data)
 
-    def get_all_unique_chars_from_traning_txtdata(self):
+    @staticmethod
+    def get_all_unique_chars_from_traning_txtdata():
         chars = ''
-        for char in list(set(self.trainig_data)):
+        for char in list(set(trainig_data)):
             chars+= char
         return chars
     
         # ' '.join(chars) # this will also so same thigs, may can be more optimal
 
-    def encode(self):
+    @staticmethod
+    def encode(input_txt: List[str]) -> List[int]:
         pass
 
-    def decoder(self):
+    @staticmethod
+    def decoder():
         pass
 
 
