fa3319b8c260d4046bc933b3f49091329f89534d
Dipak Khade <dipakhade214@gmail.com>
2026-02-27 19:27:27 +0530

    set_encoding_decoding_mapping checkpoint

--- 02_tokenizer.py
+++ 02_tokenizer.py
@@ -1,25 +1,37 @@
 from typing import List
+
 # custom simple tokenzier  #dipak
+
 class CustomTokenzier:
     trainig_data = ''
     unique_chars = ''
+
+    encoding_mapper = ''
+    decoding_mapper = ''
+
     def __init__(self, trainig_data_file_path):
         with open(trainig_data_file_path, 'r+') as f:
             content = f.read()
-            CustomTokenzier.trainig_data = content
-            CustomTokenzier.unique_chars = CustomTokenzier.get_all_unique_chars_from_traning_txtdata(CustomTokenzier.trainig_data)
+            CustomTokenzier.trainig_data: str = content
+            CustomTokenzier.unique_chars: str = CustomTokenzier.get_all_unique_chars_from_traning_txtdata()
+            CustomTokenzier.set_encoding_decoding_mapping()
 
     @staticmethod
-    def get_all_unique_chars_from_traning_txtdata():
+    def get_all_unique_chars_from_traning_txtdata() -> str:
         chars = ''
-        for char in list(set(trainig_data)):
+        for char in list(set(CustomTokenzier.trainig_data)):
             chars+= char
         return chars
     
-        # ' '.join(chars) # this will also so same thigs, may can be more optimal
 
     @staticmethod
-    def encode(input_txt: List[str]) -> List[int]:
+    def set_encoding_decoding_mapping() -> None :
+        CustomTokenzier.encoding_mapper = [ {char: index} for index, char in enumerate(CustomTokenzier.unique_chars) ]
+        CustomTokenzier.decoding_mapper = [ {index: char} for index, char in enumerate(CustomTokenzier.unique_chars) ]
+        return
+
+    @staticmethod
+    def encode(input_txt: str) -> List[int]:
         pass
 
     @staticmethod
@@ -28,7 +40,6 @@ class CustomTokenzier:
 
 
 tokenzier = CustomTokenzier('traning_input_txt.txt')
-print(tokenzier.get_all_unique_chars_from_traning_txtdata())
 
 
 #tiktoken -- Byte Pair Encoding (BPE) Tokenizer is the ting that ChatGPT uses for tokenzing the input txt
