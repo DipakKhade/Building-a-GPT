73f7f3d6c42394d2a1dcbbfb42e0ad7f6bd24c9c
Dipak Khade <dipakhade214@gmail.com>
2026-02-27 18:45:29 +0530

    add abstract custom tokenizer class

---
+++ 02_tokenizer.py
@@ -0,0 +1,25 @@
+
+# custom simple tokenzier
+class CustomTokenzier:
+    trainig_data = ''
+    def __init__(self, trainig_data):
+        pass
+
+    @staticmethod
+    def get_all_unique_chars_from_traning_txtdata():
+        pass
+
+    @staticmethod
+    def encode():
+        pass
+
+    @staticmethod
+    def decoder():
+        pass
+
+
+
+
+
+#tiktoken -- Byte Pair Encoding (BPE) Tokenizer is the ting that ChatGPT uses for tokenzing the input txt
+
