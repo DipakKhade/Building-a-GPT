77f61ae6e5d8727acc68346db54c5aecb227d8e9
Dipak Khade <dipakhade214@gmail.com>
2026-02-28 12:00:05 +0530

    added ramayana traning txt

--- 01_input_traning_data_init.py
+++ 01_input_traning_data_init.py
@@ -1,8 +1,10 @@
 import requests
 
 def download_training_data():
-    url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    res = requests.get(url=url)
+    url = 'https://www.gutenberg.org/cache/epub/7864/pg7864.txt'
+    res = requests.get(url=url, stream=True, timeout=30, headers={
+        'User-Agent': 'Mozilla/5.0'
+    })
     with open('traning_input_txt.txt', 'w+') as f:
         f.write(res.text)
     return
